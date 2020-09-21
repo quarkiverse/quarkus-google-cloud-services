20be6f676b9a1ee29896c9b7a82cd75ce742566f
Loïc Mathieu <loikeseke@gmail.com>
2020-09-21 14:18:36 +0200

    The big renamming

---
+++ integration-tests/src/test/java/io/quarkiverse/googlecloudservices/it/GoogleServicesResourcesTest.java
@@ -0,0 +1,52 @@
+package io.quarkiverse.googlecloudservices.it;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class GoogleServicesResourcesTest {
+
+    @Test
+    public void testBigQuery() {
+        given()
+                .when().get("/bigquery")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testFirestore() {
+        given()
+                .when().get("/firestore")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testPubSub() {
+        given()
+                .when().get("/pubsub")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    public void testSpanner() {
+        given()
+                .when().get("/spanner")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testStorage() {
+        given()
+                .when().get("/storage")
+                .then()
+                .statusCode(200);
+    }
+
+}
--- integration-tests/src/test/java/io/quarkus/NativeGoogleServicesResourcesIT.java
+++
@@ -1,9 +0,0 @@
-package io.quarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGoogleServicesResourcesIT extends GoogleServicesResourcesTest {
-
-    // Execute the same tests but in native mode.
-}
