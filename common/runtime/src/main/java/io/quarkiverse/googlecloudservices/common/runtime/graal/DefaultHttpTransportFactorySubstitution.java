5c88866a1990f858a9a6f93950fb9acd23c4832f
Loïc Mathieu <loikeseke@gmail.com>
2020-10-29 17:13:25 +0100

    fix: DefaultHttpTransportFactorySubstitution is not always needed
    Fixes #37

--- common/runtime/src/main/java/io/quarkiverse/googlecloudservices/common/runtime/graal/DefaultHttpTransportFactorySubstitution.java
+++ common/runtime/src/main/java/io/quarkiverse/googlecloudservices/common/runtime/graal/DefaultHttpTransportFactorySubstitution.java
@@ -1,16 +1,31 @@
 package io.quarkiverse.googlecloudservices.common.runtime.graal;
 
+import java.util.function.BooleanSupplier;
+
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 
-@TargetClass(className = "com.google.cloud.http.HttpTransportOptions$DefaultHttpTransportFactory")
+@TargetClass(className = "com.google.cloud.http.HttpTransportOptions$DefaultHttpTransportFactory", onlyWith = DefaultHttpTransportFactorySubstitution.OnlyIfInClassPath.class)
 public final class DefaultHttpTransportFactorySubstitution {
     @Substitute
     public HttpTransport create() {
         // Appengine HttpTransport didn't works on native image.
         // Anyway, appengine don't allow to deploy native image on it so it's not an issue.
         return new NetHttpTransport();
+    }
+
+    public static class OnlyIfInClassPath implements BooleanSupplier {
+        @Override
+        public boolean getAsBoolean() {
+            try {
+                Class.forName("com.google.cloud.http.HttpTransportOptions$DefaultHttpTransportFactory", false,
+                        Thread.currentThread().getContextClassLoader());
+                return true;
+            } catch (ClassNotFoundException e) {
+                return false;
+            }
+        }
     }
 }
