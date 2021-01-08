810e9ce12f4d336fdf7ab052f0b9ca556e8d3e1a
dzou <dzou@users.noreply.github.com>
2021-01-08 14:27:52 -0500

    Rely on official graalvm support for reflection configurations

--- common/deployment/src/main/java/io/quarkiverse/googlecloudservices/common/deployment/CommonBuildSteps.java
+++ common/deployment/src/main/java/io/quarkiverse/googlecloudservices/common/deployment/CommonBuildSteps.java
@@ -4,26 +4,8 @@ import io.quarkiverse.googlecloudservices.common.GcpCredentialProducer;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 public class CommonBuildSteps {
-
-    private static final String[] REGISTER_FOR_REFLECTION = {
-            "com.google.api.client.json.GenericJson",
-            "com.google.api.client.googleapis.json.GoogleJsonError",
-            "com.google.api.client.googleapis.json.GoogleJsonError$ErrorInfo",
-            "com.google.api.client.util.GenericData",
-            "com.google.api.client.json.webtoken.JsonWebSignature",
-            "com.google.api.client.json.webtoken.JsonWebToken$Payload",
-            "com.google.api.client.json.webtoken.JsonWebToken$Header",
-            "com.google.api.client.json.webtoken.JsonWebSignature$Header",
-            "com.google.api.client.json.webtoken.JsonWebToken$Payload"
-    };
-
-    @BuildStep
-    public ReflectiveClassBuildItem registerForReflection() {
-        return new ReflectiveClassBuildItem(true, true, REGISTER_FOR_REFLECTION);
-    }
 
     @BuildStep
     public AdditionalBeanBuildItem producer() {
