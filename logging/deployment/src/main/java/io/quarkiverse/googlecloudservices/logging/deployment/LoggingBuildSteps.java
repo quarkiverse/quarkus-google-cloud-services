a4cd4adb2d2f828dc92b4a6d421152ea5c2861d0
Lars J. Nilsson <contact@larsan.net>
2022-01-04 19:45:19 +0100

    making logging and default write options injectable

--- logging/deployment/src/main/java/io/quarkiverse/googlecloudservices/logging/deployment/LoggingBuildSteps.java
+++ logging/deployment/src/main/java/io/quarkiverse/googlecloudservices/logging/deployment/LoggingBuildSteps.java
@@ -1,23 +1,10 @@
 package io.quarkiverse.googlecloudservices.logging.deployment;
 
-import com.google.auth.oauth2.GoogleCredentials;
-
-import io.quarkiverse.googlecloudservices.logging.runtime.JsonFormatter;
-import io.quarkiverse.googlecloudservices.logging.runtime.LabelExtractor;
 import io.quarkiverse.googlecloudservices.logging.runtime.LoggingConfiguration;
-import io.quarkiverse.googlecloudservices.logging.runtime.TraceInfo;
-import io.quarkiverse.googlecloudservices.logging.runtime.TraceInfoExtractor;
 import io.quarkiverse.googlecloudservices.logging.runtime.cdi.LoggingProducer;
 import io.quarkiverse.googlecloudservices.logging.runtime.cdi.WriteOptionsProducer;
-import io.quarkiverse.googlecloudservices.logging.runtime.ecs.EscJsonFormat;
-import io.quarkiverse.googlecloudservices.logging.runtime.format.JsonHandler;
-import io.quarkiverse.googlecloudservices.logging.runtime.format.TextHandler;
 import io.quarkiverse.googlecloudservices.logging.runtime.recorder.LoggingHandlerFactory;
-import io.quarkiverse.googlecloudservices.logging.runtime.util.LevelTransformer;
-import io.quarkiverse.googlecloudservices.logging.runtime.util.SimpleFormatter;
-import io.quarkiverse.googlecloudservices.logging.runtime.util.StackTraceArrayRenderer;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
-import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
@@ -43,7 +30,7 @@ public class LoggingBuildSteps {
         return new AdditionalBeanBuildItem(WriteOptionsProducer.class);
     }
 
-    @BuildStep
+    /*@BuildStep
     public UnremovableBeanBuildItem credentials() {
         return UnremovableBeanBuildItem.beanTypes(GoogleCredentials.class);
     }
@@ -62,7 +49,7 @@ public class LoggingBuildSteps {
                 TextHandler.class,
                 SimpleFormatter.class,
                 StackTraceArrayRenderer.class);
-    }
+    }*/
 
     @BuildStep
     @Record(ExecutionTime.RUNTIME_INIT)
