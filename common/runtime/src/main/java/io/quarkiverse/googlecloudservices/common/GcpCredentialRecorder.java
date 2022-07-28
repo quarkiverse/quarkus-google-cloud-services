7ec878814a7fec6aaee27ffa99045b26e270326f
Lucas POUZAC <lucas.pouzac@lpoconseil.fr>
2022-07-28 15:43:23 +0200

    Add enableMetadataServer option

--- common/runtime/src/main/java/io/quarkiverse/googlecloudservices/common/GcpCredentialRecorder.java
+++ common/runtime/src/main/java/io/quarkiverse/googlecloudservices/common/GcpCredentialRecorder.java
@@ -14,7 +14,7 @@ public class GcpCredentialRecorder {
         // This ack allows to set the projectId to its default at build time as we cannot use the
         // RunTimeConfigurationDefaultBuildItem that is only usable at build time.
         // A more elegant solution would be to create a config source for this.
-        if (bootstrapConfig.projectId.isEmpty()) {
+        if (bootstrapConfig.enableMetadataServer && bootstrapConfig.projectId.isEmpty()) {
             bootstrapConfig.projectId = Optional.ofNullable(ServiceOptions.getDefaultProjectId());
         }
 
