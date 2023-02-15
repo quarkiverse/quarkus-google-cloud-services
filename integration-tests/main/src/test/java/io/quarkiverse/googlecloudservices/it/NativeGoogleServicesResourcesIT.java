c2dc8b75df216bc354e14ca65570c577e3fdbfd4
Loïc Mathieu <loikeseke@gmail.com>
2023-02-15 14:20:42 +0100

    Use @QuarkusIntegrationTest for native tests

--- integration-tests/main/src/test/java/io/quarkiverse/googlecloudservices/it/NativeGoogleServicesResourcesIT.java
+++ integration-tests/main/src/test/java/io/quarkiverse/googlecloudservices/it/NativeGoogleServicesResourcesIT.java
@@ -2,9 +2,9 @@ package io.quarkiverse.googlecloudservices.it;
 
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
-import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-@NativeImageTest
+@QuarkusIntegrationTest
 @EnabledIfSystemProperty(named = "gcloud.test", matches = "true")
 public class NativeGoogleServicesResourcesIT extends GoogleServicesResourcesTest {
     // Execute the same tests but in native mode.
