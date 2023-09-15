4801669298cbb54eba962440029283550c2ca837
zZHorizonZz <danfiala23@gmail.com>
2023-09-15 11:49:37 +0200

    enhancement(devservices): initial version of bigtable emulator

---
+++ bigtable/deployment/src/main/java/io/quarkiverse/googlecloudservices/bigtable/deployment/BigtableDevServiceProcessor.java
@@ -0,0 +1,175 @@
+package io.quarkiverse.googlecloudservices.bigtable.deployment;
+
+import io.quarkus.deployment.IsNormal;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.BuildSteps;
+import io.quarkus.deployment.builditem.CuratedApplicationShutdownBuildItem;
+import io.quarkus.deployment.builditem.DevServicesResultBuildItem;
+import io.quarkus.deployment.builditem.DevServicesSharedNetworkBuildItem;
+import io.quarkus.deployment.builditem.DockerStatusBuildItem;
+import io.quarkus.deployment.builditem.LaunchModeBuildItem;
+import io.quarkus.deployment.console.ConsoleInstalledBuildItem;
+import io.quarkus.deployment.console.StartupLogCompressor;
+import io.quarkus.deployment.dev.devservices.GlobalDevServicesConfig;
+import io.quarkus.deployment.logging.LoggingSetupBuildItem;
+import org.jboss.logging.Logger;
+import org.testcontainers.containers.BigtableEmulatorContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Processor responsible for managing Pub/Sub Dev Services.
+ * <p>
+ * The processor starts the Pub/Sub service in case it's not running.
+ */
+@BuildSteps(onlyIfNot = IsNormal.class, onlyIf = GlobalDevServicesConfig.Enabled.class)
+public class BigtableDevServiceProcessor {
+
+    private static final Logger LOGGER = Logger.getLogger(BigtableDevServiceProcessor.class.getName());
+
+    // Running dev service instance
+    private static volatile DevServicesResultBuildItem.RunningDevService devService;
+    // Configuration for the Pub/Sub Dev service
+    private static volatile BigtableDevServiceConfig config;
+
+    @BuildStep
+    public DevServicesResultBuildItem start(DockerStatusBuildItem dockerStatusBuildItem,
+                                            BigtableBuildTimeConfig pubSubBuildTimeConfig,
+                                            List<DevServicesSharedNetworkBuildItem> devServicesSharedNetworkBuildItem,
+                                            Optional<ConsoleInstalledBuildItem> consoleInstalledBuildItem,
+                                            CuratedApplicationShutdownBuildItem closeBuildItem,
+                                            LaunchModeBuildItem launchMode,
+                                            LoggingSetupBuildItem loggingSetupBuildItem,
+                                            GlobalDevServicesConfig globalDevServicesConfig) {
+        // If dev service is running and config has changed, stop the service
+        if (devService != null && !pubSubBuildTimeConfig.devservice.equals(config)) {
+            stopContainer();
+        } else if (devService != null) {
+            return devService.toBuildItem();
+        }
+
+        // Set up log compressor for startup logs
+        StartupLogCompressor compressor = new StartupLogCompressor(
+                (launchMode.isTest() ? "(test) " : "") + "Google Cloud PubSub Dev Services Starting:",
+                consoleInstalledBuildItem,
+                loggingSetupBuildItem);
+
+        // Try starting the container if conditions are met
+        try {
+            devService = startContainerIfAvailable(dockerStatusBuildItem, pubSubBuildTimeConfig.devservice,
+                    globalDevServicesConfig.timeout);
+        } catch (Throwable t) {
+            LOGGER.warn("Unable to start PubSub dev service", t);
+            // Dump captured logs in case of an error
+            compressor.closeAndDumpCaptured();
+            return null;
+        } finally {
+            compressor.close();
+        }
+
+        return devService == null ? null : devService.toBuildItem();
+    }
+
+    /**
+     * Start the container if conditions are met.
+     *
+     * @param dockerStatusBuildItem, Docker status
+     * @param config, Configuration for the Pub/Sub service
+     * @param timeout, Optional timeout for starting the service
+     * @return Running service item, or null if the service couldn't be started
+     */
+    private DevServicesResultBuildItem.RunningDevService startContainerIfAvailable(DockerStatusBuildItem dockerStatusBuildItem,
+                                                                                   BigtableDevServiceConfig config,
+                                                                                   Optional<Duration> timeout) {
+        if (!config.enabled) {
+            // PubSub service explicitly disabled
+            LOGGER.debug("Not starting Dev Services for PubSub as it has been disabled in the config");
+            return null;
+        }
+
+        if (!dockerStatusBuildItem.isDockerAvailable()) {
+            LOGGER.warn("Not starting devservice because docker is not available");
+            return null;
+        }
+
+        return startContainer(dockerStatusBuildItem, config, timeout);
+    }
+
+    /**
+     * Starts the Pub/Sub emulator container with provided configuration.
+     *
+     * @param dockerStatusBuildItem, Docker status
+     * @param config, Configuration for the PubSub service
+     * @param timeout, Optional timeout for starting the service
+     * @return Running service item, or null if the service couldn't be started
+     */
+    private DevServicesResultBuildItem.RunningDevService startContainer(DockerStatusBuildItem dockerStatusBuildItem,
+                                                                        BigtableDevServiceConfig config,
+                                                                        Optional<Duration> timeout) {
+        // Create and configure Pub/Sub emulator container
+        BigtableEmulatorContainer emulatorContainer = new QuarkusBigtableContainer(
+                DockerImageName.parse(config.imageName).asCompatibleSubstituteFor("gcr.io/google.com/cloudsdktool/cloud-sdk"),
+                config.emulatorPort.orElse(null));
+
+        // Set container startup timeout if provided
+        timeout.ifPresent(emulatorContainer::withStartupTimeout);
+        emulatorContainer.start();
+
+        // Set the config for the started container
+        BigtableDevServiceProcessor.config = config;
+
+        // Return running service item with container details
+        return new DevServicesResultBuildItem.RunningDevService(BigtableBuildSteps.FEATURE,
+                emulatorContainer.getContainerId(),
+                emulatorContainer::close, "quarkus.google.cloud.bigtable.emulator-host",
+                emulatorContainer.getEmulatorEndpoint());
+    }
+
+    /**
+     * Stops the running Pub/Sub emulator container.
+     */
+    private void stopContainer() {
+        if (devService != null && devService.isOwner()) {
+            try {
+                // Try closing the running dev service
+                devService.close();
+            } catch (Throwable e) {
+                LOGGER.error("Failed to stop pubsub container", e);
+            } finally {
+                devService = null;
+            }
+        }
+    }
+
+    /**
+     * Class for creating and configuring a PubSub emulator container.
+     */
+    private static class QuarkusBigtableContainer extends BigtableEmulatorContainer {
+
+        private final Integer fixedExposedPort;
+        private static final int INTERNAL_PORT = 8085;
+
+        private QuarkusBigtableContainer(DockerImageName dockerImageName, Integer fixedExposedPort) {
+            super(dockerImageName);
+            this.fixedExposedPort = fixedExposedPort;
+        }
+
+        /**
+         * Configures the Pub/Sub emulator container.
+         */
+        @Override
+        public void configure() {
+            super.configure();
+
+            // Expose Pub/Sub emulatorPort
+            if (fixedExposedPort != null) {
+                addFixedExposedPort(fixedExposedPort, INTERNAL_PORT);
+            } else {
+                addExposedPort(INTERNAL_PORT);
+            }
+        }
+    }
+}
--- integration-tests/main/src/main/java/io/quarkiverse/googlecloudservices/it/BigtableResource.java
+++ integration-tests/main/src/main/java/io/quarkiverse/googlecloudservices/it/BigtableResource.java
@@ -1,23 +1,29 @@
 package io.quarkiverse.googlecloudservices.it;
 
-import java.io.IOException;
-
-import jakarta.annotation.PostConstruct;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.GrpcTransportChannel;
+import com.google.api.gax.rpc.FixedTransportChannelProvider;
+import com.google.api.gax.rpc.TransportChannelProvider;
 import com.google.cloud.bigtable.admin.v2.BigtableTableAdminClient;
 import com.google.cloud.bigtable.admin.v2.BigtableTableAdminSettings;
 import com.google.cloud.bigtable.admin.v2.models.CreateTableRequest;
+import com.google.cloud.bigtable.admin.v2.stub.BigtableTableAdminStubSettings;
+import com.google.cloud.bigtable.admin.v2.stub.EnhancedBigtableTableAdminStub;
 import com.google.cloud.bigtable.data.v2.BigtableDataClient;
 import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
 import com.google.cloud.bigtable.data.v2.models.Row;
 import com.google.cloud.bigtable.data.v2.models.RowCell;
 import com.google.cloud.bigtable.data.v2.models.RowMutation;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.io.IOException;
 
 @Path("/bigtable")
 public class BigtableResource {
@@ -31,21 +37,49 @@ public class BigtableResource {
     @ConfigProperty(name = "bigtable.authenticated", defaultValue = "true")
     boolean authenticated;
 
+    @ConfigProperty(name = "quarkus.google.cloud.bigtable.emulator-host")
+    String emulatorHost;
+
     @Inject
     CredentialsProvider credentialsProvider;
 
     @PostConstruct
     void initBigtable() throws IOException {
-        BigtableTableAdminSettings.Builder settings = BigtableTableAdminSettings.newBuilder()
-                .setProjectId(projectId)
-                .setInstanceId(INSTANCE_ID);
-        if (authenticated) {
-            settings.setCredentialsProvider(credentialsProvider);
-        }
-        try (BigtableTableAdminClient adminClient = BigtableTableAdminClient.create(settings.build())) {
-            if (!adminClient.exists(TABLE_ID)) {
-                CreateTableRequest createTableRequest = CreateTableRequest.of(TABLE_ID).addFamily(COLUMN_FAMILY_ID);
-                adminClient.createTable(createTableRequest);
+        if (emulatorHost != null) {
+            ManagedChannel channel = ManagedChannelBuilder.forTarget(emulatorHost).usePlaintext().build();
+
+            TransportChannelProvider channelProvider = FixedTransportChannelProvider.create(
+                    GrpcTransportChannel.create(channel)
+            );
+            NoCredentialsProvider credentialsProvider = NoCredentialsProvider.create();
+
+            EnhancedBigtableTableAdminStub stub = EnhancedBigtableTableAdminStub.createEnhanced(
+                    BigtableTableAdminStubSettings
+                            .newBuilder()
+                            .setTransportChannelProvider(channelProvider)
+                            .setCredentialsProvider(credentialsProvider)
+                            .build()
+            );
+
+            try (BigtableTableAdminClient adminClient = BigtableTableAdminClient.create(projectId, INSTANCE_ID, stub)) {
+                if (!adminClient.exists(TABLE_ID)) {
+                    CreateTableRequest createTableRequest = CreateTableRequest.of(TABLE_ID).addFamily(COLUMN_FAMILY_ID);
+                    adminClient.createTable(createTableRequest);
+                }
+            }
+        } else {
+            BigtableTableAdminSettings.Builder settings = BigtableTableAdminSettings.newBuilder()
+                    .setProjectId(projectId)
+                    .setInstanceId(INSTANCE_ID);
+
+            if (authenticated) {
+                settings.setCredentialsProvider(credentialsProvider);
+            }
+            try (BigtableTableAdminClient adminClient = BigtableTableAdminClient.create(settings.build())) {
+                if (!adminClient.exists(TABLE_ID)) {
+                    CreateTableRequest createTableRequest = CreateTableRequest.of(TABLE_ID).addFamily(COLUMN_FAMILY_ID);
+                    adminClient.createTable(createTableRequest);
+                }
             }
         }
     }
@@ -55,6 +89,16 @@ public class BigtableResource {
         BigtableDataSettings.Builder settings = BigtableDataSettings.newBuilder()
                 .setProjectId(projectId)
                 .setInstanceId(INSTANCE_ID);
+
+        if (emulatorHost != null) {
+            settings = BigtableDataSettings.newBuilderForEmulator(emulatorHost, 8086)
+                    .setProjectId(projectId)
+                    .setInstanceId(INSTANCE_ID);
+            if (authenticated) {
+                settings.setCredentialsProvider(credentialsProvider);
+            }
+        }
+
         if (authenticated) {
             settings.setCredentialsProvider(credentialsProvider);
         }
