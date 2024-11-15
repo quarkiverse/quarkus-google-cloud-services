00e05826daa98e185e174ad6b4ab2f91f26d5fa0
karrybit <takumi.k.5610@gmail.com>
2024-11-15 23:45:49 +0900

    Use NoCredentials in case the emulator host is being connected to

--- storage/runtime/src/main/java/io/quarkiverse/googlecloudservices/storage/runtime/StorageProducer.java
+++ storage/runtime/src/main/java/io/quarkiverse/googlecloudservices/storage/runtime/StorageProducer.java
@@ -2,8 +2,11 @@ package io.quarkiverse.googlecloudservices.storage.runtime;
 
 import java.io.IOException;
 
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.core.CredentialsProvider;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Default;
+import jakarta.enterprise.inject.Instance;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -19,7 +22,7 @@ import io.quarkiverse.googlecloudservices.common.GcpConfigHolder;
 public class StorageProducer {
 
     @Inject
-    Credentials googleCredentials;
+    Instance<CredentialsProvider> credentialsProvider;
 
     @Inject
     GcpConfigHolder gcpConfigHolder;
@@ -32,6 +35,9 @@ public class StorageProducer {
     @Default
     public Storage storage() throws IOException {
         GcpBootstrapConfiguration gcpConfiguration = gcpConfigHolder.getBootstrapConfig();
+        Credentials googleCredentials = storageConfiguration.hostOverride().isPresent()
+                ? new NoCredentialsProvider().getCredentials()
+                : credentialsProvider.get().getCredentials();
         StorageOptions.Builder builder = StorageOptions.newBuilder()
                 .setCredentials(googleCredentials)
                 .setProjectId(gcpConfiguration.projectId().orElse(null));
