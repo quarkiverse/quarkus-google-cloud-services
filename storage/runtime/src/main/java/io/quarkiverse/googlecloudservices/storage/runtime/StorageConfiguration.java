b671cbef450b3f29c6d4a5e8cd8f2117738862b0
Jeroen Benckhuijsen <jeroen.benckhuijsen@gmail.com>
2025-02-25 16:20:32 +0100

    Added additional config options for Storage

--- storage/runtime/src/main/java/io/quarkiverse/googlecloudservices/storage/runtime/StorageConfiguration.java
+++ storage/runtime/src/main/java/io/quarkiverse/googlecloudservices/storage/runtime/StorageConfiguration.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+import org.threeten.bp.Duration;
 
 @ConfigMapping(prefix = "quarkus.google.cloud.storage")
 @ConfigRoot(phase = ConfigPhase.RUN_TIME)
@@ -14,4 +16,72 @@ public interface StorageConfiguration {
      * This is most commonly used for development or testing activities with a local Google Cloud Storage emulator instance.
      */
     Optional<String> hostOverride();
+
+    /**
+     * Transport options
+     */
+    Transport transport();
+
+    /**
+     * Retry options
+     */
+    Retry retry();
+
+    interface Transport {
+
+        /**
+         * Use the GRPC transport instead of the default HTTP transport.
+         */
+        @WithDefault("false")
+        Boolean useGrpc();
+
+        /**
+         * Storage HTTP transport connect timeout
+         */
+        Optional<Integer> httpConnectTimeout();
+
+        /**
+         * Storage transport read timeout
+         */
+        Optional<Integer> httpReadTimeout();
+    }
+
+    interface Retry {
+
+        /**
+         * Initial retry delay. See {@link com.google.api.gax.retrying.RetrySettings.Builder#setInitialRetryDelay(Duration)}
+         */
+        Optional<Integer> initialRetryDelayMillis();
+
+        /**
+         * Max retry delay. See {@link com.google.api.gax.retrying.RetrySettings.Builder#setMaxRetryDelay(Duration)}
+         */
+        Optional<Integer> maxRetryDelayMillis();
+
+        /**
+         * Initial RPC timeout. See {@link com.google.api.gax.retrying.RetrySettings.Builder#setInitialRpcTimeout(Duration)}
+         */
+        Optional<Integer> initialRpcTimeoutMillis();
+
+        /**
+         * Max RPC timeout. See {@link com.google.api.gax.retrying.RetrySettings.Builder#setMaxRpcTimeout(Duration)}
+         */
+        Optional<Integer> maxRpcTimeoutMillis();
+
+        /**
+         * Logical timeout. See {@link com.google.api.gax.retrying.RetrySettings.Builder#setLogicalTimeout(Duration)}
+         */
+        Optional<Integer> logicalTimeoutMillis();
+
+        /**
+         * Total timeout. See {@link com.google.api.gax.retrying.RetrySettings.Builder#setTotalTimeout(Duration)}
+         */
+        Optional<Integer> totalTimeoutMillis();
+
+        /**
+         * Maximum attempts. See {@link com.google.api.gax.retrying.RetrySettings.Builder#setMaxAttempts(int)}
+         */
+        Optional<Integer> maxAttempts();
+
+    }
 }
