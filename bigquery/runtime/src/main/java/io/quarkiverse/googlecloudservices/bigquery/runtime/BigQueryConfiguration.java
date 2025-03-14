989707fddb82e8816b161af52a1a9a06e11e8a8f
Jeroen Benckhuijsen <jeroen.benckhuijsen@gmail.com>
2025-03-14 11:27:53 +0100

    Formatter fix

--- bigquery/runtime/src/main/java/io/quarkiverse/googlecloudservices/bigquery/runtime/BigQueryConfiguration.java
+++ bigquery/runtime/src/main/java/io/quarkiverse/googlecloudservices/bigquery/runtime/BigQueryConfiguration.java
@@ -1,10 +1,10 @@
 package io.quarkiverse.googlecloudservices.bigquery.runtime;
 
+import java.util.Optional;
+
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
-
-import java.util.Optional;
 
 @ConfigMapping(prefix = "quarkus.google.cloud.bigquery")
 @ConfigRoot(phase = ConfigPhase.RUN_TIME)
