f6898752f8ea6702087787caf5aa071648b416d9
Fungrim <contact@larsan.net>
2025-10-28 10:20:57 +0100

    adding pull config for concurrency controll

---
+++ pubsub/runtime/src/main/java/io/quarkiverse/googlecloudservices/pubsub/PubSubPullConfiguration.java
@@ -0,0 +1,28 @@
+package io.quarkiverse.googlecloudservices.pubsub;
+
+import java.util.Optional;
+
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
+
+@ConfigMapping(prefix = "quarkus.google.cloud.pubsub.pull")
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
+public interface PubSubPullConfiguration {
+
+    /**
+     * Number of concurrent streams to use for pull subscriptions. Defaults
+     * to 1 which is the same as the base PubSub library.
+     */
+    @WithDefault("1")
+    Optional<Integer> parallelStreamCount();
+
+    /**
+     * Number of concurrent messages to process per stream. Defaults
+     * to 5 which is the same as the base PubSub library.
+     */
+    @WithDefault("5")
+    Optional<Integer> streamConcurrency();
+
+}
