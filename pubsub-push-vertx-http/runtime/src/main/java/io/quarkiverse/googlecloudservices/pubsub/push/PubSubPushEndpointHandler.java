f0a4c47c96589997a2c4db4f96de12069165751f
Jeroen Benckhuijsen <jeroen.benckhuijsen@gmail.com>
2026-01-02 17:58:57 +0100

    Split push logic into a separate extension to resolve dependency issues when using the module combined with google cloud functions

--- pubsub-push-vertx-http/runtime/src/main/java/io/quarkiverse/googlecloudservices/pubsub/push/PubSubPushEndpointHandler.java
+++ pubsub-push-vertx-http/runtime/src/main/java/io/quarkiverse/googlecloudservices/pubsub/push/PubSubPushEndpointHandler.java
@@ -1,9 +1,6 @@
 package io.quarkiverse.googlecloudservices.pubsub.push;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.util.Base64;
-import java.util.Map;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -12,10 +9,7 @@ import org.jboss.logging.Logger;
 
 import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import com.google.cloud.pubsub.v1.MessageReceiver;
-import com.google.protobuf.ByteString;
-import com.google.protobuf.util.Timestamps;
 import com.google.pubsub.v1.ProjectSubscriptionName;
-import com.google.pubsub.v1.PubsubMessage;
 
 import io.vertx.core.Handler;
 import io.vertx.ext.web.RoutingContext;
@@ -47,8 +41,9 @@ public class PubSubPushEndpointHandler implements Handler<RoutingContext> {
     }
 
     private void dispatchToListeners(RoutingContext rc, PubSubMessageJson message) {
-        var subscriptionName = ProjectSubscriptionName.parse(message.subscription);
-        LOGGER.debug("Received push message from subscription " + subscriptionName + " send at " + message.message.publishTime);
+        var subscriptionName = ProjectSubscriptionName.parse(message.subscription());
+        LOGGER.debug(
+                "Received push message from subscription " + subscriptionName + " send at " + message.message().publishTime());
 
         var receiver = getMessageReceiver(subscriptionName);
         if (receiver != null) {
@@ -56,14 +51,14 @@ public class PubSubPushEndpointHandler implements Handler<RoutingContext> {
                 receiver.receiveMessage(message.toPubsub(), new AckReplyConsumer() {
                     @Override
                     public void ack() {
-                        LOGGER.trace("Message receiver acknowledged the message " + message.message().messageId);
+                        LOGGER.trace("Message receiver acknowledged the message " + message.message().messageId());
                         rc.response().setStatusCode(200).end();
                     }
 
                     @Override
                     public void nack() {
                         LOGGER.warn("Message receiver indicated the message was not processed correctly. Message " +
-                                message.message().messageId + " will be failed for subscription " + subscriptionName);
+                                message.message().messageId() + " will be failed for subscription " + subscriptionName);
                         rc.fail(500);
                     }
                 });
@@ -81,51 +76,4 @@ public class PubSubPushEndpointHandler implements Handler<RoutingContext> {
         return manager.getReceiver(subscriptionName);
     }
 
-    /**
-     * Represents the JSON form of the pubsub messages received via the HTTP endpoint
-     *
-     * @param message The message
-     * @param subscription The subscription
-     */
-    private record PubSubMessageJson(
-            MessageJson message,
-            String subscription) {
-
-        PubsubMessage toPubsub() throws IOException {
-            var builder = PubsubMessage.newBuilder();
-            builder.setMessageId(message().messageId);
-            builder.setData(toByteString(decode(message.data)));
-
-            if (message.attributes != null) {
-                builder.putAllAttributes(message.attributes);
-            }
-
-            if (message.publishTime != null) {
-                try {
-                    builder.setPublishTime(Timestamps.parse(message.publishTime()));
-                } catch (ParseException e) {
-                    throw new IOException("Failed to parse publish time " + message.publishTime(), e);
-                }
-            }
-
-            return builder.build();
-        }
-
-        private String decode(String data) {
-            return new String(Base64.getDecoder().decode(data));
-        }
-
-        private ByteString toByteString(String data) {
-            return ByteString.copyFromUtf8(data);
-        }
-
-    }
-
-    private record MessageJson(
-            String messageId,
-            String publishTime,
-            String data,
-            Map<String, String> attributes) {
-
-    }
 }
